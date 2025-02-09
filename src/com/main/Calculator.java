a94c6d55335e5de07a12b2046796056717339b15
nabhat <anaghabhatsamagodu@outlook.com>
2025-02-10 00:32:25 +0530

    initial commit

---
+++ src/com/main/Calculator.java
@@ -0,0 +1,17 @@
+package com.main;
+
+
+public class Calculator {
+	public int addition(int a, int b) {
+		return a+b;
+	}
+	public int subtraction (int a, int b) {
+		return a-b;
+	}
+	public int multipy (int a, int b) {
+		return a*b;
+	}
+	public int division(int a, int b) throws ArithmeticException{
+		return a/b;
+	}
+}
