07acb4a8584c318c31d4657d27e5e9ad12db8b9d
nabhat <anaghabhatsamagodu@outlook.com>
2025-02-10 12:12:00 +0530

    Suppressed warning for unused.

--- src/com/main/CalculatorTest.java
+++ src/com/main/CalculatorTest.java
@@ -1,7 +1,38 @@
 package com.main;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class CalculatorTest {
+	@Test
 	public void addtionTest() {
-		
+		Calculator calculator = new Calculator();
+		int result = calculator.addition(1, 20);
+		assertEquals(result, 21);
+	}
+	@Test
+	public void multiplyTest() {
+		Calculator calculator = new Calculator();
+		int result = calculator.multipy(1, 20);
+		assertEquals(result, 20);
+	}
+	@Test
+	public void subtractionTest() {
+		Calculator calculator = new Calculator();
+		int result = calculator.subtraction(20, 1);
+		assertEquals(result, 19);
+	}
+	@Test
+	public void divisionTest() {
+		Calculator calculator = new Calculator();
+		int result = calculator.division(100, 20);
+		assertEquals(result, 5);
+	}
+	@Test(expected = ArithmeticException.class)
+	public void divisionTestForException() {
+		Calculator calculator = new Calculator();
+		@SuppressWarnings("unused")
+		int result = calculator.division(10, 0);
 	}
 }
